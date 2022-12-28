f12910037b069f40190e5d7b4e3476c771fc4201
Patrick Sontag <pmsontag84@gmail.com>
2022-12-28 16:52:31 -0600

    add process exit

--- step1.js
+++ step1.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 const cat = (path) => {
     fs.readFile(path, 'utf8', (err, data) => {
         if(err) {
-            console.log("ERROR", err);
-            process.kill(1)
+            console.log(`ERROR reading ${path}: \n`, err);
+            process.exit(1)
         }
         console.log("DATA...", data);
     })
