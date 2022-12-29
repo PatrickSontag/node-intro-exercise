c250f0fd2a7ed4a1e9d37b35ee8e35f5dc44eaa6
Patrick Sontag <pmsontag84@gmail.com>
2022-12-29 14:58:49 -0600

    use path and out

--- step3.js
+++ step3.js
@@ -1,27 +1,16 @@
 const fs = require('fs');
 const axios = require('axios');
 
-const cat = (path) => {
+// async function cat(path) {
+const cat = (path, out) => {
     fs.readFile(path, 'utf8', (err, data) => {
         if(err) {
             console.log(`ERROR reading ${path}: \n`, err);
             process.exit(1)
         }
-        console.log(data);
+        console.log("CAT data:", data)
     })
 }
-
-// const cat = (path) => {
-//     fs.readFile(path, 'utf8', (err, data) => {
-//         console.log("cat4")
-//         if(err) {
-//             console.log(`ERROR reading ${path}: \n`, err);
-//             process.exit(1)
-//         }
-//         console.log("cat2")
-//         // respond([data]);
-//     })
-// }
 
 const webCat = (url) => {
     axios.get(url)
@@ -30,40 +19,28 @@ const webCat = (url) => {
     })
     .catch(function (error) {
         respond([`ERROR fetching ${url}:`, error.message])
-        // console.log(`ERROR fetching ${url}: \n`, error.message);
     })
 }
 
 const respond = (data) => {
     for (d of data)
     console.log(d);
-    // console.log(data);
 }
 
-const argv2 = process.argv[2];
-const argv3 = process.argv[3];
-const argv4 = process.argv[4];
+let path;
+let out;
 
-const hasOutFlag = /--out/.test(argv2);
-const isUrl = /http/.test(argv2);
-
-const switchBoard = () => {
-    if (argv2) {
-        if (hasOutFlag) {
-            console.log("argv2:", argv2);
-            console.log("argv3:", argv3);
-            console.log("argv4:", argv4);
-            return
-        }
-        if (isUrl) {
-            webCat(argv2);
-            return
-        }
-        cat(argv2);
-        return
-    }
-    console.log("additional argument needed");
-    process.exit(1);
+if (process.argv[2] === "--out") {
+    path = process.argv[4];
+    out = process.argv[3];
+    console.log("OUT path:", path)
+    console.log("OUT out:", out)
+    cat(path, out);
+} else {
+    path = process.argv[2];
 }
-
-switchBoard();
+if (/http/.test(path)) {
+    webCat(path);
+} else {
+    cat(path)
+}
