36927fa80dbe6123c6658b3ff95afd72fea44011
Patrick Sontag <pmsontag84@gmail.com>
2022-12-29 07:24:10 -0600

    add webCat

--- step2.js
+++ step2.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const axios = require('axios');
 
 const cat = (path) => {
     fs.readFile(path, 'utf8', (err, data) => {
@@ -10,6 +11,18 @@ const cat = (path) => {
     })
 }
 
+const webCat = (url) => {
+    axios.get(url).then(function(response){
+        console.log(response.data);
+    })
+}
+
+const isUrl = /http/.test(process.argv[2]);
+
 if (process.argv[2]) {
-    cat(process.argv[2]);
+    if (isUrl) {
+        webCat(process.argv[2]);
+    } else {
+        cat(process.argv[2]);
+    }
 }
